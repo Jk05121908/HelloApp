57cd8b0d31e61ed70a3d6a26705f5ff685ffe841
Jk05121908 <kavyasmitasingh@gmail.com>
2026-05-07 01:44:17 +0530

    TTT UC2: Toss to decide first player and symbol

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -1,23 +1,51 @@
+import java.util.Random;
+
 public class TicTacToe {
     public static void main(String[] args) {
 
-        // Create a 3x3 board
+        // ===== UC1: Create and print empty board =====
         char[][] board = new char[3][3];
 
-        // Initialize all cells with '-'
+        // Initialize board
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
             }
         }
 
-        // Print the board
+        // Print board
         System.out.println("Tic Tac Toe Board:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
-            System.out.println(); // move to next line
+            System.out.println();
         }
+
+        // ===== UC2: Toss for first player =====
+        Random random = new Random();
+
+        // 0 = Player, 1 = Computer
+        int toss = random.nextInt(2);
+
+        char playerSymbol;
+        char computerSymbol;
+        String currentPlayer;
+
+        if (toss == 0) {
+            playerSymbol = 'X';
+            computerSymbol = 'O';
+            currentPlayer = "Player";
+        } else {
+            playerSymbol = 'O';
+            computerSymbol = 'X';
+            currentPlayer = "Computer";
+        }
+
+        // Display result
+        System.out.println("\n--- Toss Result ---");
+        System.out.println("Player Symbol: " + playerSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println(currentPlayer + " will start first!");
     }
 }
